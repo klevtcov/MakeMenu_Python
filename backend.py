472a520706abab5e5bb98a7072cb1ed566893203
Sergey <37954897+klevtcov@users.noreply.github.com>
2022-12-31 04:57:39 +0300

    random func testing

--- backend.py
+++ backend.py
@@ -1,1 +1,7 @@
 from ingridients import proteins, carbs, fats, fiber
+import random
+
+print(proteins[random.randrange(len(proteins))])
+print(proteins[random.randrange(len(proteins))])
+print(proteins[random.randrange(len(proteins))])
+print(proteins[random.randrange(len(proteins))])
