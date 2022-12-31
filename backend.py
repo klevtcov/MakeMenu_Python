dd5febedbda867097e0b3b7d0b9044b62f858dde
Sergey <37954897+klevtcov@users.noreply.github.com>
2022-12-31 06:14:05 +0300

    function research

--- backend.py
+++ backend.py
@@ -1,7 +1,35 @@
 from ingridients import proteins, carbs, fats, fiber
 import random
 
-print(proteins[random.randrange(len(proteins))])
-print(proteins[random.randrange(len(proteins))])
-print(proteins[random.randrange(len(proteins))])
-print(proteins[random.randrange(len(proteins))])
+def random_ingridient(ingridient, quantity):
+    result = set()
+    while len(result) < quantity:
+        result.add(ingridient[random.randrange(len(ingridient))])
+    return list(result)
+
+# print(random_ingridient(fats, 5))
+
+def make_menu(quantity):
+    result = []
+    ingridients_for_menu = []
+    ingridients_for_menu.append([
+        random_ingridient(proteins, quantity),
+        random_ingridient(carbs, quantity),
+        random_ingridient(fats, quantity),
+        random_ingridient(fiber, quantity)
+        ])
+    for i in range(quantity):
+        for ingridient in ingridients_for_menu:
+            result.append(ingridient[i])
+
+    return result
+
+
+
+print(make_menu(2))
+
+
+# print(proteins[random.randrange(len(proteins))])
+# print(proteins[random.randrange(len(proteins))])
+# print(proteins[random.randrange(len(proteins))])
+# print(proteins[random.randrange(len(proteins))])
